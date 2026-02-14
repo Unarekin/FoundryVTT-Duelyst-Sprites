d0d5fb4aa307a664c1a5c3fcc1585fa9695a04cb
Erica Phelps <erica@blackspork.com>
2026-02-14 14:06:17 -0600

    feat:Implemented assign for compendia

--- src/context.ts
+++ src/context.ts
@@ -26,23 +26,46 @@ async function confirmOverrideSections(unit: Unit, actor: Actor): Promise<ActorO
   }) as unknown as ActorOverrides;
 }
 
+Hooks.on("renderCompendium", (compendium: foundry.applications.sidebar.apps.Compendium, html: HTMLElement) => {
+  const listItems = Array.from<HTMLElement>(html.querySelectorAll(`li.entry.actor`));
+  listItems.forEach(li => {
+    const entryId = li.dataset.entryId;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const packId = (compendium as any).collection?.metadata?.id;
+
+    if (typeof entryId === "string" && typeof packId === "string") {
+      li.dataset.packId = packId;
+    }
+  })
+});
+
 Hooks.on("getActorContextOptions", (app: foundry.applications.sidebar.tabs.ActorDirectory, entries: foundry.applications.ux.ContextMenu.Entry<HTMLLIElement>[]) => {
   entries.push({
     name: "DUELYSTSPRITES.CONTEXT.ASSIGN",
     icon: `<i class="fa-solid fa-user-pen"></i>`,
     callback: (li: HTMLLIElement) => {
       if (!li.dataset.entryId) return;
-      const actor = game.actors?.get(li.dataset.entryId);
-      if (!actor) return;
 
+      let actor: Actor | undefined = undefined;
       let selectedUnit: Unit | undefined = undefined;
 
-      UnitPresetBrowserApplication
-        .browse()
+
+      // const actor = game.actors?.get(li.dataset.entryId);
+
+      (() => {
+        if (li.dataset.packId)
+          return fromUuid(`Compendium.${li.dataset.packId}.Actor.${li.dataset.entryId}`);
+        else
+          return Promise.resolve(game.actors?.get(li.dataset.entryId));
+      })().then(item => {
+        if (item) actor = item;
+        if (!item) throw new Error("Actor not found");
+        return UnitPresetBrowserApplication.browse()
+      })
         .then(unit => {
           if (unit) {
             selectedUnit = unit;
-            return confirmOverrideSections(unit, actor);
+            return confirmOverrideSections(unit, actor!);
           }
         })
         .then((overrides?: ActorOverrides | null) => {
@@ -70,7 +93,7 @@ Hooks.on("getActorContextOptions", (app: foundry.applications.sidebar.tabs.Actor
           if (overrides.spriteAnimations)
             foundry.utils.setProperty(updates, "flags.sprite-animations", selectedUnit.spriteAnimations);
 
-          return actor.update(updates);
+          return actor!.update(updates);
         })
         .catch(err => {
           console.error(err);
